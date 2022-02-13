a01f6c2db05f91cdfac4e1e6112dad39d13c7c0a
vilcinskij <vilcinskij@gmail.com>
2022-02-14 01:21:54 +0200

    blog2__

---
+++ bloggradle/src/main/java/com/itproger/bloggradle/BloggradleApplication.java
@@ -0,0 +1,13 @@
+package com.itproger.bloggradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggradleApplication.class, args);
+	}
+
+}
---
+++ bloggradle/src/test/java/com/itproger/bloggradle/BloggradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproger.bloggradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
